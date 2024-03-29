59e2e43600317256357fe09e525922c32873fa22
Juan Marmolejo <jmarmol1@my.centennialcollege.ca>
2024-03-28 22:05:25 -0400

    Add color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "primary": "#263973",
+        "secondary": "#05C7F2",
+        "third": "#6B7FF2",
+        "fourth": "#445EF2",
+        "fifth": "#010E26"
       },
     },
   },
