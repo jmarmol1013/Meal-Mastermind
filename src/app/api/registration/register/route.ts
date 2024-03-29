59de3343d797721f5bf2d78188b129dd683a2e6a
Juan Marmolejo <jmarmol1@my.centennialcollege.ca>
2024-03-29 19:06:58 -0400

    Add feature to save username cookie

--- src/app/api/registration/register/route.ts
+++ src/app/api/registration/register/route.ts
@@ -3,6 +3,7 @@ import { APIResponse } from '@typesApp/api';
 import { User } from '@typesApp/user';
 import { isUserAuthenticated } from '@utils/firebase/firebase-admin';
 import { connectMongo } from '@utils/mongo-connection';
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
@@ -22,23 +23,32 @@ export async function POST(req: NextRequest) {
         }
 
         await connectMongo();
-        const body: User = await req.json();
+        const body = await req.json();
+        const user: User = body.user;
 
-        if (body.email || body.firstName || body.lastName || body.username) {
-            body.isRegister = false;
-            const user = await Users.create(body);
+        if (user.email || user.firstName || user.lastName || user.username) {
+            user.isRegister = false;
+            user.role = 'user';
+            const userDB = await Users.create(user);
+
+            const expiresIn = 60 * 60 * 24 * 5 * 1000; // 5 days
+            cookies().set('username', user.username, {
+                maxAge: expiresIn,
+                httpOnly: true,
+                secure: true,
+            });
 
             return NextResponse.json<APIResponse<User>>({
                 statusCode: 200,
                 message: 'User save sucessfully.',
-                data: user,
+                data: userDB,
             });
         }
 
         return NextResponse.json<APIResponse<User>>(
             {
                 statusCode: 400,
-                message: 'The request body is missing required fields.',
+                message: 'The request user is missing required fields.',
             },
             {
                 status: 400,
