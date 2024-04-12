a22c6d87caa1ab9cedd5d46ea40a677243da999c
Juan Marmolejo <jmarmol1@my.centennialcollege.ca>
2024-04-12 14:31:02 -0400

    Add type for favorite recipes

--- src/types/addRecipeToUser.ts
+++ src/types/addRecipeToUser.ts
@@ -1,9 +1,13 @@
-import { ObjectId } from "mongoose"
+import { ObjectId } from 'mongoose';
 
 export type AddRecipeToUser = {
     recipes: ObjectId[];
-}
+};
 
 export type AddOneRecipeToUser = {
     recipe: ObjectId;
-}
+};
+
+export type AddOneFavRecipe = {
+    favorites: ObjectId;
+};
