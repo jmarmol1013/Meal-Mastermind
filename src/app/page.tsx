7fcb3727cc3958845e208aaf7f461b76c7fb211e
Juan Marmolejo <jmarmol1@my.centennialcollege.ca>
2024-04-18 14:36:42 -0400

    Change line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   redirect('/dashboard');
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      MealPlanner
+      MealPlanne
     </main>
   );
 }
